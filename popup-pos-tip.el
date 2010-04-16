4b224bf316bc44807f4e33e070dd24ab39045c3f
IRIE Shinsuke <irieshinsuke@yahoo.co.jp>
2010-04-17 02:11:11 +0900

    popup-pos-tip: Changed so that filling is performed only when maximum width of string exceeds the value of :width

--- popup-pos-tip.el
+++ popup-pos-tip.el
@@ -4,7 +4,7 @@
 
 ;; Author: S. Irie
 ;; Keywords: Tooltip
-;; Version: 0.1.0
+;; Version: 0.1.0.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -70,6 +70,7 @@
   (if (bufferp string)
       (setq string (with-current-buffer string (buffer-string))))
 
+  (or width (setq width popup-tip-max-width))
   (and (eq margin t) (setq margin 1))
   (or margin-left (setq margin-left (or margin 0)))
   (or margin-right (setq margin-right (or margin 0)))
@@ -77,10 +78,11 @@
        (setq point (popup-child-point parent parent-offset)))
 
   (setq rows (pos-tip-split-string string
-                                   (+ margin-left
-                                      (or width popup-tip-max-width))
+                                   (+ margin-left width)
                                    margin-left
-                                   'none)
+                                   (and (> (car (pos-tip-string-width-height string))
+                                           width)
+                                        'none))
         string (let ((rmargin-str (make-string margin-right ?\s)))
                  (propertize (concat (mapconcat 'identity
                                                 rows
