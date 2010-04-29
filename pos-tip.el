bbaed17ab5c88900e08bd1620584799e03b4a1a0
IRIE Shinsuke <irieshinsuke@yahoo.co.jp>
2010-04-29 19:23:48 +0900

    pos-tip.el: Modified old FSF address

--- pos-tip.el
+++ pos-tip.el
@@ -6,7 +6,7 @@
 ;; Maintainer: S. Irie
 ;; Keywords: Tooltip
 
-(defconst pos-tip-version "0.3.4.1")
+(defconst pos-tip-version "0.3.4.2")
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -20,8 +20,8 @@
 
 ;; You should have received a copy of the GNU General Public
 ;; License along with this program; if not, write to the Free
-;; Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-;; MA 02111-1307 USA
+;; Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+;; MA  02110-1301 USA
 
 ;;; Commentary:
 
